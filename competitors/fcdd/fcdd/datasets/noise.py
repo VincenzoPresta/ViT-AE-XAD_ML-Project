c3ba056aac70d1fc776e483ece5cc635458627da
vpresta9@gmail.com <vpresta9@gmail.com>
2025-08-21 18:20:56 +0200

    fix: aggiornata import funzione gaussian di SciPy (ora in scipy.signal.windows) per compatibilità con versioni recenti

--- competitors/fcdd/fcdd/datasets/noise.py
+++ competitors/fcdd/fcdd/datasets/noise.py
@@ -3,7 +3,7 @@ from typing import Tuple
 import numpy as np
 import torch
 from kornia import gaussian_blur2d
-from scipy import signal
+from scipy.signal import windows as signal
 from skimage.transform import rotate as im_rotate
 
 
