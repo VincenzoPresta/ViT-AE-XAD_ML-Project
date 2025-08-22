dcc95ead190436e6a625aab4254be078df436767
vpresta9@gmail.com <vpresta9@gmail.com>
2025-08-22 20:23:12 +0200

    launch experiments cifar support

--- launch_experiments.py
+++ launch_experiments.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     if args.i != 'rand':
         args.i = float(args.i)
 
-    if args.ds == 'mnist'  or args.ds == 'fmnist':
+    if args.ds in ['mnist', 'fmnist', 'cifar']:
         X_train, Y_train, X_test, Y_test, GT_train, GT_test = \
             square(args.c, perc_anom_train=args.patr, perc_anom_test=args.pate, size=args.size,
                    intensity=args.i, DATASET=args.ds, seed=args.s)
@@ -102,6 +102,8 @@ if __name__ == '__main__':
         data_path = os.path.join('datasets', args.ds, 'files')
         X_train, Y_train, X_test, Y_test, GT_train, GT_test = extract_dataset(f'datasets/{args.ds}', args.na, seed=args.s)
         ret_path = os.path.join('results', args.ds)
+        
+    
         
     print("DEBUG: args.ds =", args.ds) #DEBUG    
 
