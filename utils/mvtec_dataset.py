047ca1cb6fda9d306426210532b440b513b48a45
vpresta9@gmail.com <vpresta9@gmail.com>
2025-11-21 12:17:34 +0100

    first test augmentation

--- utils/mvtec_dataset.py
+++ utils/mvtec_dataset.py
@@ -5,7 +5,7 @@ import torch
 from torch.utils.data import Dataset
 
 import torchvision.transforms as transforms
-from utils.transforms_vit import get_vit_augmentation, get_vit_test_transform
+from transforms_vit import get_vit_augmentation, get_vit_test_transform
 
 
 class MvtecAD(Dataset):
