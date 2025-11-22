b30c764b5c821bab1811b35f122e4e7916c0ab7e
vpresta9@gmail.com <vpresta9@gmail.com>
2025-11-22 12:55:50 +0100

    fixing plot - binarized heatmap

--- aexad_script.py
+++ aexad_script.py
@@ -188,7 +188,7 @@ class Trainer:
                 plt.axis("off")
 
                 plt.subplot(2,3,5)
-                plt.imshow(binary_h, cmap="plasma", vmin=0, vmax=1)
+                plt.imshow(binary_h, cmap="cividis")
                 plt.title("Binarized heatmap")
                 plt.axis("off")
 
